c8c1e4dcc66414758221b7e71ace23e6fcd3e66a
Justin Trombley <jtrombley96@gmail.com>
2019-11-26 18:17:36 -0500

    Integers and Floats

---
+++ Lesson1.py
@@ -0,0 +1,32 @@
+# print("Justin Trombley")
+# name = input("What's your name? ")
+# print('Hello ' + name)
+
+#Fundamental Data Types
+
+#int
+print(type(2 + 4))
+print(type(2 - 4))
+print(type(2 * 4))
+print(2 **3)
+print( 5 // 2)
+print(5 % 4)
+#float
+print(type(2 / 4))
+print(type(1.1 + 9.9))
+
+
+bool
+str
+list
+tuple
+set
+dict
+
+#Classes -> custom types
+#Supercar
+
+#Specialized Data Types
+#Modules
+
+#None 
