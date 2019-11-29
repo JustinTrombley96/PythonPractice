a7d072df0127f9ef67b9f704eca8dc78b2f12775
Justin Trombley <jtrombley96@gmail.com>
2019-11-28 20:09:16 -0500

    Variables

--- Lesson1.py
+++ Lesson1.py
@@ -44,3 +44,20 @@ dict
 #binary representation
 print(bin(5))
 print(int('0b101', 2))
+
+#variables
+user1_iq = 180
+user2_iq = 190
+user3_iq = 210 
+user2_lowered_iq = user2_iq / 2
+a, b, c, = 1, 2, 3
+print(a, b, c)
+print(a)
+print(b)
+print(c)
+print(user2_iq)
+print(user2_lowered_iq)
+
+
+#constants are all in caps
+PI = 3.14
